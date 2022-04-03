6f4eee485ffb70edd96ef23e8681ff756ebfaf5f
md sawkat hossain <saikat@workspacify.com>
2022-04-03 16:45:42 +0600

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+/* eslint-env node */
+module.exports = {
+  "root": true,
+  "extends": [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended"
+  ],
+  "env": {
+    "vue/setup-compiler-macros": true
+  },
+  "overrides": [
+    {
+      "files": [
+        "cypress/integration/**.spec.{js,ts,jsx,tsx}"
+      ],
+      "extends": [
+        "plugin:cypress/recommended"
+      ]
+    }
+  ]
+}
