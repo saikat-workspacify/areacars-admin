5a0a49bdd68c658bebfb056679eb1c7d8800f590
Hasibur Rahman <hasib.active@gmail.com>
2022-04-05 03:05:55 +0600

    updated

--- src/views/pages/ReservationPage.vue
+++ src/views/pages/ReservationPage.vue
@@ -81,7 +81,10 @@ const form = reactive({
    countryVersion: '',
    deliveryDeadline: '',
 })
-
+const dummy = reactive({
+   trueValue: true,
+   falseValue: false
+})
 const areas = [
    { label: 'PKW', value: 'PKW' },
    { label: 'MPH', value: 'MPH' },
@@ -320,12 +323,252 @@ const colors = [
 
          <div class="row">
             <div class="col-md-6">
-               <p class="text-muted">Own equipment features</p>
+               <div class="ms-5">
+                  <p class="text-muted fw-medium" style="font-size: 14px;">Own equipment features</p>
+
+                  <CheckBox v-model="dummy.falseValue" label="Export - net sales contracts" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Warranty possible on request" class="mt-2" />
+                  <CheckBox v-model="dummy.trueValue" label="Financing also possible without down payment" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Registration service" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Trade-in of your old car" class="mt-2" />
+
+               </div>
             </div>
 
             <div class="col-md-5 offset-md-1 ">
-
+               <div class="me-5 mt-4">
+                  <CheckBox v-model="dummy.falseValue" label="Delivery possible" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="In-house car garage" class="mt-2" />
+                  <CheckBox v-model="dummy.trueValue" label="Tire and wheel change" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="The above information is without guarantee. They do not represent an agreed condition. Errors and prior sale reserved!" class="mt-2" />
+               </div>
             </div>
+         </div>
+
+         <hr class="mx-5 my-4">
+
+         <!-- Drive, brakes, chassis, steering, tires -->
+         <div class="row">
+            <div class="col-md-6">
+               <div class="ms-5">
+                  <p class="text-muted fw-medium" style="font-size: 14px;">Drive, brakes, chassis, steering, tires</p>
+
+                  <CheckBox v-model="dummy.falseValue" label="Adaptive Damper System (ADS)" class="mt-2" />
+                  <CheckBox v-model="dummy.trueValue" label="All-wheel drive" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="All-weather tires" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Automatic limited slip differential (ASD)" class="mt-2" />
+                  <CheckBox v-model="dummy.trueValue" label="Brake energy regeneration" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Dual clutch transmission" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Electronic Damper Control" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Electronic chassis control" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Hybrid drive (electric motor)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Hydroactive chassis" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Ceramic brakes" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Light alloy wheels" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Dynamic steering" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Air suspension" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Level control" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Level control (with compressor)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Emergency brake function" class="mt-2" />
+               </div>
+            </div>
+
+            <div class="col-md-5 offset-md-1 ">
+               <div class="me-5 mt-4">
+                  <CheckBox v-model="dummy.trueValue" label="Emergency wheel" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Breakdown kit" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Electric parking brakes" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Tires with run-flat properties" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Spare wheel" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Wheels/Wide tires" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Disc brakes" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Power steering" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Summer tires" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Limited slip differential" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Sport suspension" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Stabilizer from the top / rear" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Steel rims" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Tirefit" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Winter tires" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Electron. Differential lock (EDS)" class="mt-2" />
+               </div>
+            </div>
+         </div>
+
+         <hr class="mx-5 my-4">
+
+         <!-- Roof -->
+         <div class="row">
+            <div class="col-md-6">
+               <div class="ms-5">
+                  <p class="text-muted fw-medium" style="font-size: 14px;">Roof</p>
+
+                  <CheckBox v-model="dummy.falseValue" label="Roof antenna" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: pop-up roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: Roof hatch" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: Folding roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: Normal" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: Sliding/tilting roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: Sliding roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: Targa" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: automatic" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: electric" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: mechanical" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof type: immovable" class="mt-2" />
+
+               </div>
+            </div>
+
+            <div class="col-md-5 offset-md-1 ">
+               <div class="me-5 mt-4">
+                  <CheckBox v-model="dummy.falseValue" label="Roof rails" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Roof rack" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Glass roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="High roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Metal roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Panorama roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Tarpaulin (soft top cover)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Solar system" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Solar roof" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Fabric roof" class="mt-2" />
+
+               </div>
+            </div>
+         </div>
+
+         <hr class="mx-5 my-4">
+
+         <!-- Entertainment/Kommunikation -->
+         <div class="row">
+            <div class="col-md-6">
+               <div class="ms-5">
+                  <p class="text-muted fw-medium" style="font-size: 14px;">Entertainment/Kommunikation</p>
+
+                  <CheckBox v-model="dummy.falseValue" label="AUX-IN-Anschluss (AUX-IN)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Android Auto" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Apple CarPlay" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Bildschirm(e)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Bluetooth" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="CD" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="CD-Wechsler" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="DVD" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="DVD-Wechsler" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Freisprecheinrichtung" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Freisprecheinrichtung Bluetooth" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Freisprecheinrichtung vorbereitet" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Kassette" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="MP3" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="MP3-Schnittstelle" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Mobiltelefon/Handy vorbereitet" class="mt-2" />
+
+               </div>
+            </div>
+
+            <div class="col-md-5 offset-md-1 ">
+               <div class="me-5 mt-4">
+                  <CheckBox v-model="dummy.falseValue" label="Multifunktionslenkrad" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Musikstreaming integriert" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Radio" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Radio digital (DAB)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Rear Seat Entertainment" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="SD-Slot" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Soundsystem" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Soundsystem (mit Fembedienung)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="TV" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Telefon" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Telefon (mit Freisprecheinrichtung)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Touchscreen" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="USB-Anschluss" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="WLAN" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="iPod-Schnittstelle" class="mt-2" />
+
+               </div>
+            </div>
+         </div>
+
+         <hr class="mx-5 my-4">
+
+         <!-- Fahrerassistenz und Informationssysteme -->
+         <div class="row">
+            <div class="col-md-6">
+               <div class="ms-5">
+                  <p class="text-muted fw-medium" style="font-size: 14px;">Entertainment/Kommunikation</p>
+
+                  <CheckBox v-model="dummy.falseValue" label="Abstandstempomat" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Antiblockiersystem (ABS)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Antischlupfregelung (ASR)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Auffahrwarnsystem" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Auffahrwarnsystem mit Bremsfunktion" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Aufmerksamkeitsassistent" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Aussentemperaturanzeige" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Bergabfahrhilfe" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Berganfahrhilfe" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Bordcomputer" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Bremsassistent" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Check-Control-System" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Digitale Instrumentierung" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Drehzahlmesser" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Einparkautomatik (selbstlenkend)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Einparkhilfe 360 grad Kamera" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Einparkhilfe Rückfahrkamera" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Einparkhilfe optisch" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Einparkhilfe-Sensor hinten" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Einparkhilfe-Sensor vorn" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Elektronisches Stabilitätsprogramm (ESP)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Fahrerinformationssystem (FIS)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Fahrprofilauswahl" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Frontkamera" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Geschwindigkeits-Begrenzeranlage" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Geschwindigkeitslimit-Assistent" class="mt-2" />
+
+               </div>
+            </div>
+
+            <div class="col-md-5 offset-md-1 ">
+               <div class="me-5 mt-4">
+                  <CheckBox v-model="dummy.falseValue" label="Headup Display" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Infotainment" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Keyless-Go" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Multifunktionsanzeige" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Müdigkeitserkennung" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Nachsichthilfe" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Navigationssystem" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Navigationssystem (mit TV-Funktion)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Navigationssystem (mobil)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Parklückendetektor" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Parktronic System (PTS)" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Reifendruckanzeige" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Reifendruckverlust-Warnung" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Speedlimiterkennung" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Sprachbediensystem" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Spurhalteassistent" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Spurwechselassistent" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Start/Stop-Automatik" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Start/Stop-Knopf" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Tempolimitanzeige" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Tempomat" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Tempomat mit Bremsfunktion" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Totwinkelassistent" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Traktionskontrolle" class="mt-2" />
+                  <CheckBox v-model="dummy.falseValue" label="Verkehrszeichenerkennung" class="mt-2" />
+
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <!-- Free Text -->
+      <div class="page-content mt-4">
+         <h6>Free-text</h6>
+
+         <div class="mt-4">
+            <textarea class="form-control" rows="12" placeholder="Write about the reservation ,Max. 3000 Zeichen "></textarea>
+         </div>
+
+         <div class="mt-4 mb-2 d-flex justify-content-end align-items-center">
+            <button type="button" class="btn btn-sm bg-transparent px-5">Save</button>
+            <button type="button" class="btn btn-sm btn-light mx-3 px-5">Cancel</button>
+            <button type="button" class="btn btn-sm btn-primary px-5">Upload</button>
          </div>
       </div>
    </default-layout>
